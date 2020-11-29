433e808e69c2e28f4a9b643c19b9a08f27978370
Gideon Kreitzer <ontwikkel@n86k.com>
2020-11-28 20:57:40 -0500

    build carousel-1 component

---
+++ src/components/carousels/1/_.js
@@ -0,0 +1,35 @@
+'use strict';
+
+export default ({
+    delay = 5000,
+    slide: slideClass = '.slide',
+    active: activeClass = '.active',
+    container = '.carousel-1',
+}) => {
+
+    // first check if the specified container is in document
+    if (!document.querySelector(container)) return null;
+
+    // retrieve the DOM element of the container selector
+    container = document.querySelector(container);
+
+    // corral all the slides that's in the slideshow container
+    const slides = container.querySelectorAll(slideClass);
+    const activeElement = container.querySelector(activeClass);
+    const numberOfSlides = slides.length;
+
+    // locate the index of the slide earmarked for initial display
+    let indexOfActiveSlide = [].indexOf.call(slides, activeElement);
+
+    // the slide rotator
+    const rotate = () => {
+        slides[indexOfActiveSlide].classList.remove('active');
+        // modulo shifts to next slide in queue and restarts at end
+        indexOfActiveSlide = ++indexOfActiveSlide % numberOfSlides;
+        slides[indexOfActiveSlide].classList.add('active');
+    };
+
+    // begin rotation
+    setInterval(rotate, delay);
+
+};
